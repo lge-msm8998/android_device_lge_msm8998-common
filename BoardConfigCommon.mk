691d25720cbe9dc4860b520723393866fe26e8c3
Han Sol Jin <hansol@hansol.ca>
2024-05-14 21:10:43 -0700

    [DNM] permissive
    Change-Id: I6eb3987cc1f7f26ba4bdbd0bb1954261da877a12

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -95,6 +95,7 @@ BOARD_KERNEL_CMDLINE += androidboot.configfs=true androidboot.usbcontroller=a800
 BOARD_KERNEL_CMDLINE += loop.max_part=7 androidboot.hardware=qcom
 BOARD_KERNEL_CMDLINE += androidboot.fstab_suffix=qcom
 BOARD_KERNEL_CMDLINE += androidboot.boot_devices=soc/1da4000.ufshc
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
