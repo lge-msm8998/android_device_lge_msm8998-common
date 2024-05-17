d07d7eff277535843e0892d49a8eaec5114afc51
Han Sol Jin <hansol@hansol.ca>
2024-05-16 19:58:37 -0700

    msm8998-common: decommonize vibrator
    - joan uses ImmVibe
    - phoenix_sprout uses QPNP
    
    Change-Id: Icfc12b1afafaca59dd1755ea339fca70e10bbe5d

--- msm8998-common.mk
+++ msm8998-common.mk
@@ -430,10 +430,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.dual_role_usb
 
-# Vibrator
-PRODUCT_PACKAGES += \
-    android.hardware.vibrator-service.lge
-
 # VNDK
 PRODUCT_PACKAGES += \
     libui_shim.vendor \
