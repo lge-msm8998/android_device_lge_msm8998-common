5f3901ed286210ae36878495dea1243051fa0469
Han Sol Jin <hansol@hansol.ca>
2024-06-24 23:39:10 -0700

    joan: remove unused audio_effects_tune.xml
    This one is used by eagle_pipeline, which was dropped in Change-Id
    I89d46c106d8ddb9f43d67559c39f664b20de2664
    
    Change-Id: I224556757aaa54456bf24b7d758ea142e69c1ae5

--- device.mk
+++ device.mk
@@ -66,7 +66,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
-    $(DEVICE_PATH)/audio/audio_effects_tune.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects_tune.xml \
     $(DEVICE_PATH)/audio/audio_output_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_output_policy.conf \
     $(DEVICE_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(DEVICE_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
