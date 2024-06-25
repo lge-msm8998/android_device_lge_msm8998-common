70aa84f346e95964bc28d1dbb83459ca5d59fa83
Han Sol Jin <hansol@hansol.ca>
2024-06-25 01:46:52 -0700

    joan: drop IMS symlinks
    IMS doesn't work on AOSP for this device.
    
    Change-Id: Ie964acb7924fba5419e87c7dcd9f9f3477618678

--- Android.mk
+++ Android.mk
@@ -102,16 +102,6 @@ $(HDCPSRM_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 ALL_DEFAULT_INSTALLED_MODULES += $(HDCPSRM_SYMLINKS)
 # END HDCPSRM Images
 
-IMS_LIBS := libimscamera_jni.so libimsmedia_jni.so
-IMS_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR_APPS)/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
-$(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "IMS lib link: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf /vendor/lib64/$(notdir $@) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(IMS_SYMLINKS)
-
 # Create a link for the WCNSS MAC firmware, which ends up as a writable
 # version in /data/misc/wifi
 # Wifi
